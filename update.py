a884beb52b17a6829c89679bad53d5261ff2017a
stepanenko-ya <stepanenko.yana.87@gmail.com>
2021-03-19 16:22:24 +0200

    new version

--- update.py
+++ update.py
@@ -12,21 +12,21 @@ db = conn.cursor()
 # f_csv = open('/home/stepanenko/Projects/test/test/qwerty.csv', 'r')
 
 # db.execute("copy dbo." + tab_num + " from '/home/stepanenko/Projects/test/test/qwerty.csv' DELIMITER '{' QUOTE E'\b' CSV")
-
-s = '{'
-db.execute(f"""select * from dbo.t203 WHERE  refno  LIKE '%{s}%' """)
-print(db.fetchall())
-conn.commit()
+#
+# s = '{'
+# db.execute(f"""select * from dbo.t203 WHERE  refno  LIKE '%{s}%' """)
+# print(db.fetchall())
+# conn.commit()
 # db.execute(f"update dbo.{tab_num} set column_1 = replace (column_1, '🔶', '@')")
 
 
 
-# def update():
-# #
-# #     # for obliqua in obliqua_lst:
-# #         # tab = version.tables.get(obliqua[:3])
-#     slash = '{'
-#     db.execute(f"update dbo.t203 set RefNo = replace (RefNo, '🔶', '{slash}')")
-#     conn.commit()
+def update():
 #
-# update()
+#     # for obliqua in obliqua_lst:
+#         # tab = version.tables.get(obliqua[:3])
+    slash = '{'
+    db.execute(f"update dbo.t203 set RefNo = replace (RefNo, '🔶', '{slash}')")
+    conn.commit()
+
+update()
